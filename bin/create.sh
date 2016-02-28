3a077d1ee5f040deb8990cdaddc1bc99a599eb91
dbuarque <diogenes.buarque@gmail.com>
2016-02-28 13:33:58 -0300

    add support for linux, osx and cygwin

--- bin/create.sh
+++ bin/create.sh
@@ -6,6 +6,26 @@ else
     cp ./config/templates/lambda.config.tpl.js ./config/$APP.config.js
     cp ./config/templates/lambda.tpl.js ./src/lambdas/$APP.js
     cp ./config/templates/lambda-spec.tpl.js ./tests/$APP-spec.js
-    sed -i '' "s/APP_NAME/$APP/g" ./tests/$APP-spec.js
-    echo "Lambda function created with sucess!"
+
+    case "$(uname -s)" in
+
+       Darwin)
+         sed -i '' "s/APP_NAME/$APP/g" ./tests/$APP-spec.js
+         ;;
+
+       Linux)
+         sed -i "s/APP_NAME/$APP/g" ./tests/$APP-spec.js
+         ;;
+
+       CYGWIN*|MINGW32*|MSYS*)
+         sed -i "s/APP_NAME/$APP/g" ./tests/$APP-spec.js
+         ;;
+
+       *)
+         echo 'Unsupported platform'
+         ;;
+    esac
+    echo "============================================="
+    echo " Lambda function created with sucess!"
+    echo "============================================="
 fi
